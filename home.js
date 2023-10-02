871d5f3e7ee2cb68198dccd14eeb83d63cc6cf61
zaktmt <130027586+zaktmt@users.noreply.github.com>
2023-10-02 12:55:15 -0500

    Update home.js

--- home.js
+++ home.js
@@ -4,25 +4,6 @@ $(document).ready(function() {
 }
 
 );
-//hover over button
-$(document).ready(function() {
-    $("#darkmode").hover(function() {
-        $("#darkmode").css("background-color", "black");
-    }, function() {
-        $("#darkmode").css("background-color", "white");
-    });
-}
-);
-//click button
-$(document).ready(function() {
-    $("#darkmode").click(function() {
-        $("body").fadeOut(1000);
-        setTimeout(function() {
-            window.location.href = "darkmode.html";
-        }, 1000);
-    });
-}
-);
 
 
 
